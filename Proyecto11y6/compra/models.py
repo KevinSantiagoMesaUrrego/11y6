b8f83d3fd2425e882a22bd092de68aec1532a372
william00936 <wsamado0@soy.sena.edu.co>
2023-04-27 12:20:07 -0500

    foraneas con botones especiales

--- Proyecto11y6/compra/models.py
+++ Proyecto11y6/compra/models.py
@@ -3,12 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-class Compra(models.Model):
-        fecha_compra= models.DateField(verbose_name="Fecha Compra", help_text="MM/DD/AAAA")
-class Detalle_compra(models.Model):
-        cantidad_producto=models.IntegerField(max_length=45,verbose_name="Cantidad Producto")
-        valor_total=models.FloatField(max_length=45,verbose_name="Valor Total Compra")
-
 class  Proveedor(models.Model):
         class Tipo_identificacion(models.TextChoices):
                 CEDULA='CC',_("Cédula")
@@ -30,7 +24,37 @@ class  Proveedor(models.Model):
                 self.correo_proveedor= self.correo_proveedor.lower()
         def __str__(self):
                 return "%s %s"%(self.nombre_proveedor,self.apellido_proveedor)
+        class Meta:
+                verbose_name_plural = "Proveedores"
+class Compra(models.Model):
+        fecha_compra= models.DateField(verbose_name="Fecha Compra", help_text="MM/DD/AAAA")
+        id_proveedor=models.ForeignKey(Proveedor, on_delete=models.CASCADE,verbose_name="Nombre Proveedor")
+        def __str__(self):
+                return "%s"%(self.id_proveedor)
+        def __str__(self):
+                return "%s "%(self.fecha_compra)
+        class Meta:
+                verbose_name_plural = "Compras"
+class Detalle_compra(models.Model):
+        cantidad_producto=models.IntegerField(max_length=45,verbose_name="Cantidad Producto")
+        valor_total=models.FloatField(max_length=45,verbose_name="Valor Total Compra")
+        def __str__(self):
+                return "%s %s"%(self.cantidad_producto,self.valor_total)
 
+        class Meta:
+                verbose_name_plural = "detalles_compras"
+class Compra(models.Model):
+        fecha_compra= models.DateField(verbose_name="Fecha Compra", help_text="MM/DD/AAAA")
+        id_proveedor=models.ForeignKey(Proveedor, on_delete=models.CASCADE,verbose_name="Nombre Proveedor")
+        id_detalle_compra=models.ForeignKey(Detalle_compra, on_delete=models.CASCADE,verbose_name="Fecha detalle compra")
+        def __str__(self):
+                return "%s"%(self.id_proveedor)
+        def __str__(self):
+                return "%s "%(self.fecha_compra)
+        def __str__(self):
+                return "%s"%(self.id_detalle_compra)
+        class Meta:
+                verbose_name_plural = "Compras"
 class Evento(models.Model):
         nombre_evento= models.CharField(max_length=45,verbose_name="Nombre Evento") 
         fecha_inicio= models.DateField(verbose_name="Fecha Incio Evento", help_text="MM/DD/AAAA")
@@ -40,6 +64,9 @@ class Evento(models.Model):
                 ACTIVO='1',_("Activo")
                 INACTIVO='0',_("Inactivo")
         estado_evento=models.CharField(max_length=1,choices=Estado_evento.choices,default=Estado_evento.ACTIVO,verbose_name="Estado Evento")
+        id_compra=models.ForeignKey(Compra, on_delete=models.CASCADE,verbose_name="Fecha de Compra")
+        def __str__(self):
+                return "%s"%(self.id_compra)
 
 
 
--- Proyecto11y6/venta/models.py
+++ Proyecto11y6/venta/models.py
@@ -1,32 +1,42 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
-class Venta(models.Model):
-    fecha_venta=models.DateField(verbose_name="fecha venta",help_text="MM/DD/AAAA")
-    def __str__(self):
-        return " %s"%(self.fecha_venta)
-    class Meta:
-        verbose_name_plural = "ventas"
-
-
 class Detalle_venta(models.Model):
     cantidad_producto=models.IntegerField(verbose_name="Cantidad Producto")
+    valor_total=models.FloatField(max_length=45,verbose_name="Valor Total venta")
     def __str__(self):
-        return " %s"%(self.cantidad_producto)
+        return " %s %s"%(self.cantidad_producto,self.valor_total)
     class Meta:
         verbose_name_plural = "detalle_ventas"
-class Reserva(models.Model):
-        fecha_reserva=models.DateField(verbose_name="Fecha Reserva",help_text="MM/DD/AAAA")
-        hora_inicio=models.DateTimeField(verbose_name="Hora de Inicio",help_text="HH/MM/SS")
-        hora_fin=models.DateTimeField(verbose_name="Hora de fin",help_text="HH/MM/SS")
-        costo=models.FloatField()
-        abono=models.FloatField()
-        fecha_abono=models.DateField(verbose_name="Fecha Abono",help_text="MM/DD/AAAA")
-        pago_total=models.FloatField()
-        fecha_pago_total=models.DateField(verbose_name="Fecha Pago Total",help_text="MM/DD/AAAA")
+
+class Venta(models.Model):
+    fecha_venta=models.DateField(verbose_name="fecha venta",help_text="MM/DD/AAAA")
+    detalle_venta_cantidad_producto=models.ForeignKey(Detalle_venta, on_delete=models.CASCADE,verbose_name="detalle venta cantidad producto")
+    def __str__(self):
+        return "%s "%(self.detalle_venta_cantidad_producto)
 class Ubicacion(models.Model):
     nombre=models.CharField(max_length=45,verbose_name="Nombre")
     class Estado(models.TextChoices):
         ACTIVO='1',_("Activo")
         INACTIVO='0',_("Inactivo")
-    estado=models.CharField(max_length=1,choices=Estado.choices,verbose_name="Estado",default=Estado.ACTIVO)
+    estado=models.CharField(max_length=1,choices=Estado.choices,verbose_name="Estado",default=Estado.ACTIVO)        
+    def __str__(self):
+        return "%s "%(self.nombre)
+    class Meta:
+        verbose_name_plural = "ubicaciones"
+
+class Reserva(models.Model):
+    fecha_reserva=models.DateField(verbose_name="Fecha Reserva",help_text="MM/DD/AAAA")
+    hora_inicio=models.DateTimeField(verbose_name="Hora de Inicio",help_text="HH/MM/SS")
+    hora_fin=models.DateTimeField(verbose_name="Hora de fin",help_text="HH/MM/SS")
+    costo=models.FloatField()
+    abono=models.FloatField()
+    fecha_abono=models.DateField(verbose_name="Fecha Abono",help_text="MM/DD/AAAA")
+    pago_total=models.FloatField()
+    fecha_pago_total=models.DateField(verbose_name="Fecha Pago Total",help_text="MM/DD/AAAA")
+    ubicacion_nombre=models.ForeignKey(Ubicacion, on_delete=models.CASCADE,verbose_name="ubicación nombre")
+    def __str__(self):
+        return "%s "%(self.ubicacion_nombre)
+    detalle_venta_cantidad_producto=models.ForeignKey(Detalle_venta, on_delete=models.CASCADE,verbose_name="detalle venta cantidad producto")
+    def __str__(self):
+        return "%s "%(self.detalle_venta_cantidad_producto)
