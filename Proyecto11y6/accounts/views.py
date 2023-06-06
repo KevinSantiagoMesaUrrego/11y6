9049a8f65bc7c35841c751b7b4011e3f0e9c1233
william00936 <wsamado0@soy.sena.edu.co>
2023-06-06 18:16:23 -0500

    Se habilita Cerrar Sesión

--- Proyecto11y6/accounts/views.py
+++ Proyecto11y6/accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render,redirect
 from .forms import CustomUserCreationForm
 from accounts.models import Register
+from django.contrib.auth import logout
+
 
 def register(request):
     if request.method == 'POST':
@@ -15,5 +17,22 @@ def register(request):
             return render(request, 'partials/register.html', {'form': form})
     else:
         form = CustomUserCreationForm()
-        return render(request, 'partials/register.html', {'form': form})
+        return render(request, 'registration/login.html', {'form': form})
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('inicio')
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def vista_protegida(request):
+    usuario = request.user
+    rol = usuario.rol  # Asume que el modelo de Usuario tiene un campo "rol"
+
+    context = {
+        'usuario': usuario,
+        'rol': rol,
+    }
+    return render(request, 'index.html', context)
+
 
