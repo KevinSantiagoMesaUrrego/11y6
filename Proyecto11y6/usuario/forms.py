1cb16b54764d261cb743c50d5565bb2477d22fa3
william00936 <wsamado0@soy.sena.edu.co>
2023-06-28 13:37:23 -0500

    Trasnparencias en tablas y formularios crear, RAUL GAY

--- Proyecto11y6/usuario/forms.py
+++ Proyecto11y6/usuario/forms.py
@@ -3,11 +3,19 @@ from django import forms
 from django.forms import ModelForm, widgets
 from usuario.models import Persona,Eps,Turno,Trabajador
 
-class PersonaForm(ModelForm):
+
+class PersonaForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            #visible.field.widget.attrs['placeholder'] = visible.field.label
+
     class Meta:
         model = Persona
         fields = "__all__"
         exclude=["estado"]
+
 class PersonaUpdateForm(ModelForm):
     class Meta:
         model = Persona
