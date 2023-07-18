28bfb920a3549fcda8a4a4754e2d56c0ff3620d1
william00936 <wsamado0@soy.sena.edu.co>
2023-07-18 17:37:30 -0500

    Formularios iguales

--- Proyecto11y6/usuario/forms.py
+++ Proyecto11y6/usuario/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from django.forms import ModelForm, widgets
-from usuario.models import Persona,Eps,Turno,Trabajador
+from usuario.models import Persona, Eps, Turno, Trabajador
 
 
 class PersonaForm(forms.ModelForm):
@@ -9,59 +9,68 @@ class PersonaForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
-            #visible.field.widget.attrs['placeholder'] = visible.field.label
+            # visible.field.widget.attrs['placeholder'] = visible.field.label
 
     class Meta:
         model = Persona
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
+
 
 class PersonaUpdateForm(ModelForm):
     class Meta:
         model = Persona
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
+
 
 class EpsForm(ModelForm):
-
     class Meta:
         model = Eps
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
+
+
 class EpsUpdateForm(ModelForm):
     class Meta:
         model = Eps
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
+
 
 class TurnoForm(ModelForm):
     class Meta:
         model = Turno
         fields = "__all__"
-        widgets={
-            'fecha_ingreso': widgets.DateInput(attrs={'type':'datetime-local'},format='%Y-%m-%dT%H:%M'),
-            'fecha_salida': widgets.DateInput(attrs={'type':'datetime-local'},format='%Y-%m-%dT%H:%M'),
+        widgets = {
+            'fecha_ingreso': widgets.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+            'fecha_salida': widgets.DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
         }
-        exclude=["estado",]
+        exclude = ["estado", ]
+
+
 class TurnoUpdateForm(ModelForm):
     class Meta:
         model = Turno
         fields = "__all__"
-        widgets={
-            'fecha_ingreso': widgets.DateTimeInput(attrs={'type':'datetime-local'},format='%Y-%m-%dT%H:%M'),
-            'fecha_salida': widgets.DateTimeInput(attrs={'type':'datetime-local'},format='%Y-%m-%dT%H:%M'),
+        widgets = {
+            'fecha_ingreso': widgets.DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+            'fecha_salida': widgets.DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
         }
-        exclude=["estado",]
+        exclude = ["estado", ]
+
+
 class TrabajadorForm(ModelForm):
-    
     class Meta:
         model = Trabajador
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
 
         # fields= ["nombre","apellido","documento","tipoDocumento"]
+
+
 class TrabajadorUpdateForm(ModelForm):
     class Meta:
         model = Trabajador
         fields = "__all__"
-        exclude=["estado"]
+        exclude = ["estado"]
