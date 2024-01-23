60b3b083f8bdb9053524f9840dd796471f75e594
KevinSantiagoMesaUrrego <ksmesa0@soy.sena.edu.co>
2024-01-23 13:48:02 -0500

    mysql client borrado

--- base/settings.py
+++ base/settings.py
@@ -93,18 +93,19 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 
 DATABASES = {
-     #'default': {
-    #   'ENGINE': 'django.db.backends.sqlite3',
-     #    'NAME': BASE_DIR / 'db.sqlite3',
-     #}
     'default': {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "read_default_file": 'my.cnf',
-            "init_command": "SET default_storage_engine=INNODB",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "OPTIONS": {
+    #         "read_default_file": 'my.cnf',
+    #         "init_command": "SET default_storage_engine=INNODB",
+    #     },
+    # }
 }
+
 
 
 
